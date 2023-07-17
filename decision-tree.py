4fbc3c3d19dd05082f50fe3368b7b3279390ff90
Arjun Maudhoo <apm526@nyu.edu>
2023-07-17 16:20:40 -0400

    Add branch creation method

--- decision-tree.py
+++ decision-tree.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 
 class DecisionTree:
 
@@ -9,10 +10,26 @@ class DecisionTree:
         self.right = None
 
     def fit(self, data, target):
+        if self.depth <= self.max_depth: print(f"currently at Depth: {self.depth}")
         self.data = data
         self.target = target
         self.independent = self.data.columns.tolist()
         self.independent.remove(target)
+        if self.depth <= self.max_depth:
+            self.__validate_data()
+            self.impurity_score = self.__calculate_impurity_score(self.data[self.target])
+            self.criteria, self.split_feature, self.information_gain = self.__find_best_split()
+            if self.criteria is not None and self.information_gain > 0: self.__create_branches()
+        else:
+            print("Max Depth Reached")
+    
+    def __create_branches(self):
+        self.left = DecisionTree(max_depth = self.max_depth, depth = self.depth + 1)
+        self.right = DecisionTree(max_depth = self.max_depth, depth = self.depth + 1)
+        left_rows = self.data[self.data[self.split_feature] <= self.criteria]
+        right_rows = self.date[self.data[self.split_feature] > self.critera]
+        self.left.fit(data = left_rows, target = self.target)
+        self.right.fit(data = right_rows, target = self.target)
 
     def __calculate_impurity_score(self, data):
         if data is None or data.empty: return 0
